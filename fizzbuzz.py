a59bc307e2535b89a690d4c0f1214ffd077afe78
kramanau <kramanauskas112@gmail.com>
2022-02-23 22:00:52 -0500

    fizz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,16 @@
-def 
+def fizzbuzz(n):
+    output = []
+    for i in range(1,n+1):
+        if not (i % 3) and not (i % 5):
+            output.append("FizzBuzz")
+        elif not (i % 3):
+            output.append("Fizz")
+        elif not (i % 5):
+            output.append("Buzz")
+        else:
+            output.append(i)
+    return output
+
+
+if __name__ == "__main__":
+    print(fizzbuzz(15))
